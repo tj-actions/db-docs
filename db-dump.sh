2c85c3755dc13db50a570631b5c22d3fd0c07a2a
Tonye Jack <jtonye@ymail.com>
2024-04-25 19:47:23 -0600

    Update db-dump.sh

--- db-dump.sh
+++ db-dump.sh
@@ -1,1 +1,7 @@
+#!/usr/bin/env bash
 
+set -euo pipefail
+
+echo "Generating schema dump..."
+pg_dump "$DB_URL" --schema-only | sed -E 's/double precision/float8/g; s/::character varying/''/g; s/::[a-zA-Z0-9_]+\.*[a-zA-Z0-9_]*//g' > dump.sql
+echo "Generated schema dump"
